40f455190f6988315bde9d77ba0e470563a6468f
Resfadz <45738287+Resfadz@users.noreply.github.com>
2020-03-02 16:02:58 +0100

    Fichiers documents et bdd

---
+++ CD.java
@@ -0,0 +1,25 @@
+package persistance;
+
+import mediatek2020.items.*;
+
+public class CD implements Document {
+    @Override
+    public void reserver(Utilisateur utilisateur) throws ReservationException {
+
+    }
+
+    @Override
+    public void emprunter(Utilisateur utilisateur) throws EmpruntException {
+
+    }
+
+    @Override
+    public void rendre(Utilisateur utilisateur) throws RetourException {
+
+    }
+
+    @Override
+    public Object[] data() {
+        return new Object[0];
+    }
+}
